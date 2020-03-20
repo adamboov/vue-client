583c4a7ffd325a892cc317716ee1d8461ae268de
adamboov <48423629+adamboov@users.noreply.github.com>
2020-03-20 16:48:43 +0800

    由底部按钮来控制路由显示

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,34 @@
 <template>
   <div id="app">
     <router-view></router-view>
+    <van-tabbar route>
+      <van-tabbar-item
+        replace
+        to="/"
+        icon="home-o"
+      >
+      首页
+      </van-tabbar-item>
+      <van-tabbar-item
+        replace
+        to="/add"
+        icon="plus"
+      >
+      发布
+      </van-tabbar-item>
+    </van-tabbar>
   </div>
 </template>
 
 <script>
-import { Button } from 'vant'
+import { Button, Tabbar, TabbarItem } from 'vant'
 
 export default {
   name: 'App',
   components: {
-    [Button.name] : Button
+    [Button.name] : Button,
+    [Tabbar.name] : Tabbar,
+    [TabbarItem.name] : TabbarItem
   }
 }
 </script>
